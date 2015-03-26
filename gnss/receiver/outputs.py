7c67769a2c9d3d619e7ed77865f3ee206e827c19
Brian Breitsch <brianbreitsch@gmail.com>
2015-03-26 16:12:01 -0600

    updated vis to bokeh

--- gnss/receiver/outputs.py
+++ gnss/receiver/outputs.py
@@ -2,6 +2,7 @@
 
 import numpy
 import matplotlib.pyplot as pyplot
+from bokeh.plotting import GridPlot, VBox, HBox, figure
 
 class TrackingOutputBuffer:
     
@@ -15,23 +16,35 @@ class TrackingOutputBuffer:
     
     def push(self, **outputs):
         for key in outputs:
-            i = self.indices[key] = self.indices[key] + 1
+            i = self.indices[key]
             self.buffers[key][i % self.outputs[key]['size']] = outputs[key]
+            self.indices[key] = self.indices[key] + 1
     
     def clear(self):
         for key in self.outputs:
             self.buffers[key][:] = 0.
             self.indices[key] = 0
             
-    def plot(self):
+    def plot(self, library='matplotlib'):
         keys = self.outputs.keys()
         n = len(keys)
+        fig = None
         rows = cols = int(numpy.ceil(numpy.sqrt(n)))
-        fig = pyplot.figure()
-        for i, key in enumerate(keys, 1):
-            ax = fig.add_subplot(rows, cols, i)
-            ax.plot(self.buffers[key])
-            ax.set_title(key)
-        return fig
+        if library is 'matplotlib':
+            fig = fig if fig else pyplot.figure()
+            for i, key in enumerate(keys, 1):
+                ax = fig.add_subplot(rows, cols, i)
+                ax.plot(self.buffers[key])
+                ax.set_title(key)
+            return fig
+        elif library is 'bokeh':
+            plots = []
+            for key in self.outputs.keys():
+                plot = figure(title=key, plot_width=250, plot_height=250, tools="pan,wheel_zoom,box_zoom,reset,save")
+                plot.line(numpy.arange(self.outputs[key]['size']), self.buffers[key], size=12, alpha=0.7)
+                plots.append(plot)
+            grid = GridPlot(children=[plots], title="tracking outputs")
+            return grid
+        return None
     
     
